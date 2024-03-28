d41e0ebfd87e07ff81d79797ac181cf261e6a39e
ivan <ivanjose_04@hotmail.com>
2024-03-28 15:21:54 -0400

    change color when enabled

--- app/src/main/java/com/example/solar_v2/AlienSolarSystem.java
+++ app/src/main/java/com/example/solar_v2/AlienSolarSystem.java
@@ -123,11 +123,11 @@ public class AlienSolarSystem extends View {
         planetas.add(new AstreCeleste(1, "Mercurio", 40, "#9e9e9e", 0, "mercurio", 100, 100));
         planetas.add(new AstreCeleste(2, "Venus", 60, "#e1c699", 0, "venus", 200, 200));
         planetas.add(new AstreCeleste(3, "Tierra", 70, "#6da7d9", 1, "tierra", 300, 300));
-        planetas.add(new AstreCeleste(4, "Marte", 50, "#ff5733", 0, "marte", 400, 400));
-        planetas.add(new AstreCeleste(5, "Júpiter", 140, "#c79b63", 0, "jupiter", 500, 500));
-        planetas.add(new AstreCeleste(6, "Saturno", 120, "#ead9a4", 0, "saturno", 600, 600));
+        planetas.add(new AstreCeleste(4, "Marte", 50, "#ff5733", 1, "marte", 400, 400));
+        planetas.add(new AstreCeleste(5, "Júpiter", 140, "#c79b63", 1, "jupiter", 500, 500));
+        planetas.add(new AstreCeleste(6, "Saturno", 120, "#ead9a4", 1, "saturno", 600, 600));
         planetas.add(new AstreCeleste(7, "Urano", 100, "#a4e4eb", 0, "urano", 700, 700));
-        planetas.add(new AstreCeleste(8, "Neptuno", 100, "#4973a6", 1, "neptuno", 800, 800));
+        planetas.add(new AstreCeleste(8, "Neptuno", 100, "#4973a6", 0, "neptuno", 800, 800));
         planetas.add(new AstreCeleste(9, "Plutón", 30, "#d3c1d0", 0, "pluton", 900, 900));
 
         for (AstreCeleste planeta : planetas) {
@@ -166,7 +166,11 @@ public class AlienSolarSystem extends View {
             }
 
             if (planeta.isTocado()) {
-                paint.setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFCCCCCC));
+                if (planeta.getState() == 1) {
+                    paint.setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFF00FF00));
+                } else {
+                    paint.setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFFFFFFF));
+                }
             } else {
                 paint.setColorFilter(null);
             }
